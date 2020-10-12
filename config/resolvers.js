255edbe5f80f3fd75892514acd4ea0d2e311516b
luis-ITO-mendoza <luisito250899@gmail.com>
2020-10-12 18:20:54 -0500

    feat(Resolvers): mutation to create new User

--- config/resolvers.js
+++ config/resolvers.js
@@ -1,6 +1,36 @@
+const userModel = require('../models/User');
+const bcryptjs = require('bcryptjs');
+
 const resolvers = {
     Query: {
-        getSomething: () => 'Something'
+        getUsers: () => {
+            
+        }
+    },
+    Mutation: {
+        newUser: async(_, { input }) => {
+            // Destructuring input
+            const { email, password } = input;
+            
+            // Check if the user is already registered
+            const userExist = await userModel.findOne({email}); 
+            if(userExist){
+                throw new Error('The user is already registered');
+            }
+            
+            // Hash password
+            input.password = await bcryptjs.hash(password, 10);
+            
+            // Save in db
+            try {
+                const newUser = new userModel(input);
+                newUser.save();
+                return newUser;
+            } catch (error) {
+                throw new Error(error);
+            }
+
+        }
     }
 };
 
