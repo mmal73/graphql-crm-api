aaae61d1715ae5ffa59606dddd86773b78f4a6ab
luis-ITO-mendoza <luisito250899@gmail.com>
2020-10-13 12:41:47 -0500

    feat(Schema): Mutation to deleteProduct

--- db/schema.js
+++ db/schema.js
@@ -49,6 +49,7 @@ const typeDefs = gql`
         # Products
         newProduct(input: ProductInput): Product
         updateProduct(id: ID!, input: ProductInput!): Product
+        deleteProduct(id: ID!): String
     }
 `;
 
