a6f2eac44c6bc4f6253a15195f8a9d6e29611d34
luis-ITO-mendoza <luisito250899@gmail.com>
2020-10-13 18:57:05 -0500

    feat: Pass authenticated user by context

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./db/schema');
-const resolvers = require('./db/resolvers');
-const conectDB = require('./config/db');
+const { ApolloServer } = require( 'apollo-server' );
+const typeDefs = require( './db/schema' );
+const resolvers = require( './db/resolvers' );
+const jwt = require( 'jsonwebtoken' );
+const conectDB = require( './config/db' );
+require( 'dotenv' ).config( { path: '.env' } );
 
 conectDB();
 
 const apolloServer = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context:( { req } ) => {
+        const token = req.headers[ 'authorization' ] || '';
+        
+        if(token){
+            try {
+                const currentUser = jwt.verify(token.replace( 'Bearer ', '' ), process.env.SECRET_WORD);
+                return{
+                    currentUser
+                }
+            } catch (error) {
+                throw new Error( error );
+            }
+        }
+    }
 });
 
 apolloServer.listen().then(({url}) => {
